3700bdb545b392ef1a0f6919a52535214a09c114
emilrichardo <emilrichardo@gmail.com>
2026-02-06 11:05:15 -0300

    deno  create client

--- supabase/functions/sync-aleph/index.ts
+++ supabase/functions/sync-aleph/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "jsr:@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // Environment variables
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL") ?? "";
