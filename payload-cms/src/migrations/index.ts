6d0ee867cbbc93480c0458145c78ed5cb0a2654c
emilrichardo <emilrichardo@gmail.com>
2026-02-19 19:42:03 -0300

    feat: add initial Payload CMS database schema, migrations, and type definitions.

---
+++ payload-cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260219_223816_payload_init from './20260219_223816_payload_init';
+
+export const migrations = [
+  {
+    up: migration_20260219_223816_payload_init.up,
+    down: migration_20260219_223816_payload_init.down,
+    name: '20260219_223816_payload_init'
+  },
+];
