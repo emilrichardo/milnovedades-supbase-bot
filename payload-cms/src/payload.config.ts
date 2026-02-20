a96bc15e26544776beb359daaecd154ca7694c11
emilrichardo <emilrichardo@gmail.com>
2026-02-20 14:28:36 -0300

    fix(payload): activar push de esquema a la base de datos temporalmente

--- payload-cms/src/payload.config.ts
+++ payload-cms/src/payload.config.ts
@@ -29,7 +29,7 @@ export default buildConfig({
       connectionString: process.env.DATABASE_URL || '',
     },
     schemaName: 'payload',
-    push: false,
+    push: true,
   }),
   sharp,
   plugins: [],
